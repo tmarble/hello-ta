8f16a496be1829340818b44a18725434aa39b0aa
Tom Marble <tmarble@info9.net>
2022-06-28 14:59:24 -0500

    fixes style errors

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
+"Simple Technical Agility Web Server"
 
-import os
 import logging
 import http.server
 import socketserver
 
-PORT = 8000
-
 class TechnicalAgilityHandler(http.server.SimpleHTTPRequestHandler):
-    
+    "Specialized version of the web server"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory='./resources/public')
 
-Handler = TechnicalAgilityHandler
+def main():
+    "The main method"
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    logging.warning("Starting Technical Agility on port %d" % PORT)
-    httpd.serve_forever()
+    port = 8000
+    Handler = TechnicalAgilityHandler
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        msg = f"Starting Technical Agility on {port=}"
+        logging.warning(msg)
+        httpd.serve_forever()
 
-    
+if __name__ == '__main__':
+    main()
